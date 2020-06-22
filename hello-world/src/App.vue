7e3bc26419684ab84f5e64f87bf268d48b7d8c19
Artem <agbusko@gmail.com>
2020-06-22 23:57:14 +0300

    AB: add radio, checkbox

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-survey></app-survey>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppSurvey from './components/AppSurvey.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppSurvey
   }
 }
 </script>
--- hello-world/src/components/AppSurvey.vue
+++ hello-world/src/components/AppSurvey.vue
@@ -1,14 +1,36 @@
 <template>
 	<div id="survey">
 		<h1>ОПРОС</h1>
+		
+		<el-radio-group v-model="radio">
+			<el-radio :label="3">Option A</el-radio>
+			<el-radio :label="6">Option B</el-radio>
+			<el-radio :label="9">Option C</el-radio>
+		</el-radio-group>
+		
+		<el-checkbox-group v-model="checkList">
+			<el-checkbox label="Option A"></el-checkbox>
+			<el-checkbox label="Option B"></el-checkbox>
+			<el-checkbox label="Option C"></el-checkbox>
+		</el-checkbox-group>
 	</div>
 </template>
 <script>
     export default {
-        name: "AppSurvey"
+        name: "AppSurvey",
+
+        data () {
+            return {
+                radio: 6,
+                checkList: ['Option A','Option B']
+            }
+        }
     }
 </script>
 <style scoped>
+	h1 {
+		color: cadetblue;
+	}
 </style>
 
 
